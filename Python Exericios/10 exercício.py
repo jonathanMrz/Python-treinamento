41560caa87ba1754600e4990389d2bee277655c1
JonathanMrz <Jonathanmorozenviski@gmail.com>
2024-03-02 11:42:09 -0300

    mudança

--- Python Exericios/10 exercício.py
+++ Python Exericios/10 exercício.py
@@ -79,6 +79,23 @@ print("-"*20)
 print(f"Maiores de idade: {mdi}\nHomens cadastrados: {hms}\nMulheres com menos de 20 anos: {mmqv}")
 print("="*10,"| Fim do Programa |","="*10)
 #Quinta questão[=================================================================]
-#Sexta questão[==================================================================]
-#Sétima questão[=================================================================]
-#Oitava questão[=================================================================]
+nm0=cnt0=pmc=""
+preco0=soma=mqm=pdmc=0
+while True:
+    nm0=str(input("Escreva o nome do produto: "))
+    preco0=int(input("Informe o preço do produto: "))
+    if preco0>1000:
+        mqm+=1
+    if soma==0 or preco0 > pdmc:
+        pmc=nm0
+        pdmc=preco0
+    soma+=preco0
+    cnt0=str(input("[S/N]Quer adicionar mais um produto? ")).strip()
+    while cnt0 not in "SsNn":
+        print("Digite uma resposta valida.")
+        cnt0=str(input("[S/N]Quer adicionar mais um produto? ")).strip()
+        if cnt0 in "Nn":
+            break
+    if cnt0 in "Nn":
+        break
+print(f"A soma de todos produtos: {soma}\nQuantidade de produtos com o preço acima de 1000: {mqm}\nProduto mais caro: {pmc}")
