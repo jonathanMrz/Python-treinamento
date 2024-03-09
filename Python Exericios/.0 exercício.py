87e516396d35e988c3d67b83b43fa4019fbbe1a6
JonathanMrz <Jonathanmorozenviski@gmail.com>
2024-03-09 15:57:14 -0300

    atualização

--- Python Exericios/.0 exercício.py
+++ Python Exericios/.0 exercício.py
@@ -4,23 +4,4 @@
 #Quarta questão[=================================================================]
 #Quinta questão[=================================================================]
 #Sexta questão[==================================================================]
-#Somente para fazer exercícios muito longos de forma separada
-cnt=objeto=valor=0
-while True:
-    objeto=input("Digite o nome de um produto: ").strip()
-    valor=int(input("Digite o valor deste produto: "))
-    cnt=input("[S/N]Deseja adcionar mais um valor? ").strip().upper()
-    while cnt not in "SN":
-        print("Digite um valor valido!")
-        cnt=input("[S/N]Deseja adicionar mais um valor? ").strip().upper()
-        if cnt in "SN":
-            break
-    if cnt in "Nn":
-        break
-lista=("Caneta",1.50,"Borracha",0.75,"Lapiz",1.00)
-
-for pos in range(0,len(lista)):
-    if pos%2==0:
-        print(f"{lista[pos]:.<30}",end=" ")
-    else:
-        print(f"R${lista[pos]:<30}")
+#Somente para fazer exercícios muito longos de forma separado
--- Python Exericios/11 exercício.py
+++ Python Exericios/11 exercício.py
@@ -18,10 +18,10 @@ print("Posição do time Corinthians: {}º lugar".format((timesfut.index("Corint
 #Terceira questão[===============================================================]
 import random as rd
 numtupla=(rd.randint(0,20),
-          rd.randint(0,20),
-          rd.randint(0,20),
-          rd.randint(0,20),
-          rd.randint(0,20))
+        rd.randint(0,20),
+        rd.randint(0,20),
+        rd.randint(0,20),
+        rd.randint(0,20))
 print(numtupla)
 print(f"Maior valor randomizado: {max(numtupla)}\nMenor valor randomizado: {min(numtupla)}")
 #Quarta questão[=================================================================]
@@ -38,4 +38,64 @@ for n in snumlist:
     if n%2==0:
         print(n,end=" ")
 #Quinta questão[=================================================================]
+nv=oq=cnt=objeto=valor=0
+lista=[]
+while True:
+    print("=-"*30)
+    objeto=input("Digite o nome de um produto: ").strip()
+    valor=float(input("Digite o valor deste produto: "))
+    lista.append(objeto);lista.append(valor)
+    print("=-"*30)
+    cnt=input("[S/N]Deseja adcionar mais um valor?\nResposta: ").strip().upper()
+    while cnt[0] not in "SN":
+        print("Digite um valor valido!")
+        cnt=input("[S/N]Deseja adicionar mais um valor?\nResposta: ").strip().upper()
+        if cnt[0] in "SN":
+            break
+    if cnt[0] in "N":
+        break
+print("=-"*30)
+cnt=input("[S/N]Deseja alterar algo da lista?\nResposta: ").upper().strip()
+while cnt[0] not in "SN":
+    print("Digite um valor válido!")
+    cnt=input("[S/N]Deseja alterar algo da lista?\nResposta: ").upper().strip()
+    if cnt[0] in "SN":
+        break
+print("=-"*30)
+while cnt[0] in "S":
+    for alt in range(0,len(lista)):
+        if alt%2==0:
+            print(f"[{lista.index(lista[alt])}] {lista[alt]:.<30}",end=" ")
+        else:
+            print(f"R${lista[alt]}")
+    oq=int(input("Digite o valor do produto que deseja alterar: "))
+    print("=-"*30)
+    print(f"{lista[oq]:.<30}R${lista[oq+1]}")
+    nv=int(input("""Digite oque deseja fazer:
+    [0]Para mudar o nome
+    [1]Para mudar o valor
+    [2]Para deletar
+Resposta:"""))
+    print("=-"*30)
+    if nv==0:
+        lista[oq]=input("Digite o novo nome do produto: ")
+    elif nv==1:
+        lista[oq+1]=float(input("Digite o novo valor do produto: "))
+    elif nv==2:
+        del(lista[oq+1]);del(lista[oq])
+    print("=-"*30)
+    cnt=input("[S/N]Deseja alterar algo da lista?\nResposta: ").upper().strip()
+    while cnt[0] not in "SN":
+        print("Digite um valor válido!")
+        cnt=input("[S/N]Deseja alterar algo da lista?\nResposta: ").upper().strip()
+        if cnt[0] in "SN":
+            break
+    if cnt[0] in "N":
+        break
+print("=-"*30)
+for pos in range(0,len(lista)):
+    if pos%2==0:
+        print(f"{lista[pos]:.<30}",end="")
+    else:
+        print(f"R${lista[pos]}")
 #Sexta questão[==================================================================]
